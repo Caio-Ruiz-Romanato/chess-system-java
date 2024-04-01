ad189c31aa31aac07a43627ea5db6d12477b39c9
Caio-Ruiz-Romanato <caioruizromanato@gmail.com>
2024-04-01 20:10:30 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,14 @@
 package application;
 
+import bordergame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("TESTE");
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 	}
 
 }
